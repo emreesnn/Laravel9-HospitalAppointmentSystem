c7d16562003a2d09bb24fe6a8c0ba10c14755c92
emreesnn <emreesnn17@gmail.com>
2022-04-24 15:43:26 +0300

    Web Template Implemention

---
+++ resources/views/home/slider.blade.php
@@ -0,0 +1,52 @@
+<!-- Start Slider -->
+<section id="mu-slider">
+    <!-- Start single slider item -->
+    <div class="mu-slider-single">
+        <div class="mu-slider-img">
+            <figure>
+                <img src="{{asset('assets')}}/img/slider/1.jpg" alt="img">
+            </figure>
+        </div>
+        <div class="mu-slider-content">
+            <h4>Welcome To Varsity</h4>
+            <span></span>
+            <h2>We Will Help You To Learn</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor amet error eius reiciendis eum sint unde eveniet deserunt est debitis corporis temporibus recusandae accusamus.</p>
+            <a href="#" class="mu-read-more-btn">Read More</a>
+        </div>
+    </div>
+    <!-- Start single slider item -->
+    <!-- Start single slider item -->
+    <div class="mu-slider-single">
+        <div class="mu-slider-img">
+            <figure>
+                <img src="{{asset('assets')}}/img/slider/2.jpg" alt="img">
+            </figure>
+        </div>
+        <div class="mu-slider-content">
+            <h4>Premiumu Quality Free Template</h4>
+            <span></span>
+            <h2>Best Education Template Ever</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor amet error eius reiciendis eum sint unde eveniet deserunt est debitis corporis temporibus recusandae accusamus.</p>
+            <a href="#" class="mu-read-more-btn">Read More</a>
+        </div>
+    </div>
+    <!-- Start single slider item -->
+    <!-- Start single slider item -->
+    <div class="mu-slider-single">
+        <div class="mu-slider-img">
+            <figure>
+                <img src="{{asset('assets')}}/img/slider/3.jpg" alt="img">
+            </figure>
+        </div>
+        <div class="mu-slider-content">
+            <h4>Exclusivly For Education</h4>
+            <span></span>
+            <h2>Education For Everyone</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor amet error eius reiciendis eum sint unde eveniet deserunt est debitis corporis temporibus recusandae accusamus.</p>
+            <a href="#" class="mu-read-more-btn">Read More</a>
+        </div>
+    </div>
+    <!-- Start single slider item -->
+</section>
+<!-- End Slider -->
--- resources/views/layouts/frontbase.blade.php
+++ resources/views/layouts/frontbase.blade.php
@@ -1,20 +1,63 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>App Name - @yield('title')</title>
-    @yield('head')
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield("title")</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
+
+    <!-- Font awesome -->
+    <link href="{{asset('assets')}}/css/font-awesome.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="{{asset('assets')}}/css/bootstrap.css" rel="stylesheet">
+    <!-- Slick slider -->
+    <link rel="stylesheet" type="text/css" href="{{asset('assets')}}/css/slick.css">
+    <!-- Fancybox slider -->
+    <link rel="stylesheet" href="{{asset('assets')}}/css/jquery.fancybox.css" type="text/css" media="screen" />
+    <!-- Theme color -->
+    <link id="switcher" href="{{asset('assets')}}/css/theme-color/default-theme.css" rel="stylesheet">
+
+    <!-- Main style sheet -->
+    <link href="{{asset('assets')}}/css/style.css" rel="stylesheet">
+
+
+    <!-- Google Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,300,300italic,500,700' rel='stylesheet' type='text/css'>
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    @yield("head")
 </head>
 <body>
-<h1>Header</h1>
+
+<!--START SCROLL TOP BUTTON -->
+<a class="scrollToTop" href="#">
+    <i class="fa fa-angle-up"></i>
+</a>
+<!-- END SCROLL TOP BUTTON -->
+
+@include("home.header")
+
 
 @section('sidebar')
-    This is the master sidebar.
+    @include("home.sidebar")
 @show
 
-<div class="container">
-    @yield('content')
-</div>
+@section('slider')
+@show
 
-<h1>Footer</h1>
+@yield('content')
+
+@include("home.footer")
 @yield('foot')
 </body>
 </html>
