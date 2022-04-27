6fafa23bbea9e13dd204e9747bc98442a0a3558e
emreesnn <emreesnn17@gmail.com>
2022-04-28 02:07:19 +0300

    Category Get and List Data from Database

--- app/Http/Controllers/AdminPanel/CategoryController.php
+++ app/Http/Controllers/AdminPanel/CategoryController.php
@@ -16,7 +16,10 @@ class CategoryController extends Controller
     public function index()
     {
         //
-        return view('admin.category.index');
+        $data= Category::all();
+        return view('admin.category.index',[
+            'data'=>$data
+        ]);
     }
 
     /**
@@ -46,6 +49,7 @@ class CategoryController extends Controller
         $data->description = $request->description;
         $data->status = $request->status;
         $data->save();
+        return redirect('admin/category');
     }
 
     /**
--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -15,6 +15,46 @@
             </div>
             <!--End Page Header -->
         </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Category List
+            </div>
+            <!-- /.panel-heading -->
+            <div class="panel-body">
+                <div class="table-responsive table-bordered">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Title</th>
+                            <th>Keywords</th>
+                            <th>Description</th>
+                            <th>Image</th>
+                            <th>Status</th>
+                            <th style="width: 40px">Edit</th>
+                            <th style="width: 40px">Delete</th>
+                            <th style="width: 40px">Show</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($data as $rs)
+                        <tr>
+                            <td>{{$rs->id}}</td>
+                            <td>{{$rs->title}}</td>
+                            <td>{{$rs->keywords}}</td>
+                            <td>{{$rs->description}}</td>
+                            <td>{{$rs->image}}</td>
+                            <td>{{$rs->status}}</td>
+                            <td><a href="/admin/category/edit{{$rs->id}}"class="btn btn-info">Edit</a></td>
+                            <td><a href="/admin/category/delete{{$rs->id}}"class="btn btn-danger">Delete</a></td>
+                            <td><a href="/admin/category/show{{$rs->id}}"class="btn btn-success">Show</a></td>
+                        </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
 
     </div>
     <!-- end page-wrapper -->
